140616dc64e96bf069f2911a7bf6dc6364bd2a79
Chris Purta <cpurta@kochava.com>
2017-10-13 08:10:08 -0700

    Change concordance to use test targets
    Allowing concordance to be a measure of the percent difference in log
    loss between the validation and test sets. This assumes that the test
    set gathered from the competition data will have test targets. We are
    only pulling a random 1/4 of the test set as a comparision against the
    submission data. This is so that the "data leakage" mentioned in the pdf
    is avoided.

--- concordance.py
+++ concordance.py
@@ -5,168 +5,53 @@ import os
 import functools
 
 # Third Party
-from sklearn.cluster import MiniBatchKMeans
-from scipy.stats import ks_2samp
+from sklearn.metrics import log_loss
 import numpy as np
 import pandas as pd
 from bson.objectid import ObjectId
 
-def has_concordance(P1, P2, P3, c1, c2, c3, threshold=0.12):
-    """Checks that the clustered submission data conforms to a concordance threshold
+def has_concordance(validation_targets, submission_val, test_targets, submission_test, threshold=0.1):
+    """Checks that the validation and test submission data have roughly equivalent log_losses based on
 
     Paramters:
     ----------
-    P1 : ndarray
-        Sorted validation submission probabilities based on the id
+    validation_targets : ndarray
+        the target values of the validation data in the competition
 
-    P2 : ndarray
-        Sorted test submission probabilities based on the id
+    submission_val : ndarray
+        the submission probabilities that are used against the validation targets using the log_loss function
 
-    P3 : ndarray
-        Sorted live submission probabilities based on the id
+    test_targets : ndarray
+        the target values of the test data in the competition
 
-    c1 : ndarray
-        Clustered validation from the tournament data
+    submission_test : ndarray
+        the submission probabilities that are used against the test targets using the log_loss function
 
-    c2 : ndarray
-        Clustered test from the tournament data
-
-    c3 : ndarray
-        Clustered live from the tournament data
-
-    threshold : float, optional, default: 0.12
-        The threshold in which our mean ks_score has to be under to have "concordance"
+    threshold : float, optional, default: 0.1
+        The threshold in which our percent difference in log loss between validation and test data has to be under to have "concordance"
 
     Returns:
     --------
     concordance : bool
         Boolean value of the clustered submission data having concordance
     """
-    ks = []
-    for i in set(c1):
+    if len(validation_targets) != len(submission_val):
+        raise ValueError('submisison validation probabilities must be the same length as competition validation data')
+    elif len(test_targets) != len(submission_test):
+        raise ValueError('submisison test probabilities must be the same length as competition test data')
 
-        ks_score = max(ks_2samp(P1.reshape(-1)[c1==i], P2.reshape(-1)[c2==i])[0],
-                       ks_2samp(P1.reshape(-1)[c1==i], P3.reshape(-1)[c3==i])[0],
-                       ks_2samp(P3.reshape(-1)[c3==i], P2.reshape(-1)[c2==i])[0])
+    val_log_loss = log_loss(validation_targets,submission_val)
+    test_log_loss = log_loss(test_targets, submission_test)
 
-        ks.append(ks_score)
-    logging.getLogger().info("Noticed score {}".format(np.mean(ks)))
-    return np.mean(ks)<threshold
+    # score is the percent difference of the val_log_loss and test_log_loss
+    score = 2.0 * (val_log_loss - test_log_loss) / (val_log_loss + test_log_loss)
 
-def make_clusters(X, X_1, X_2, X_3):
-    """Split submission data into 3 clusters using K-Means clustering
-
-    Parameters:
-    -----------
-    X: ndarray
-        tournament data for the competition round
-
-    X_1: ndarray
-        sorted validation data ids from tournament data
-
-    X_2: ndarray
-        sorted test ids data from tournament data
-
-    X_3: ndarray
-        sorted live ids data from tournament data
-
-    Returns:
-    --------
-    c1: nparray
-        Clustered validation data
-    c2: nparray
-        Clustered test data
-    c3: nparray
-        Cluster live data
-    """
-    logging.getLogger().info("New competition, clustering dataset")
-    kmeans = MiniBatchKMeans(n_clusters=5)
-
-    kmeans.fit(X)
-    c1, c2, c3 = kmeans.predict(X_1), kmeans.predict(X_2), kmeans.predict(X_3)
-    logging.getLogger().info("Finished clustering")
-    return c1, c2, c3
+    # if % difference is less than or equal to .1, call it good
+    return score <= threshold
 
 @functools.lru_cache(maxsize=2)
-def get_ids(filemanager, competition_id):
-    """Gets the ids from submission data based on the competition_id
-
-    Parameters:
-    -----------
-    filemanager : FileManager
-        S3 Bucket data access object for querying competition datasets
-    competition_id : int
-        The numerical id of the competition
-
-    Returns:
-    --------
-    val : list
-        List of all ids in the 'validation' dataset for the competition_id round
-
-    test : list
-        List of all ids in the 'test' dataset for the competition_id round
-
-    live : list
-        List of all ids in the 'live' dataset for the competition_id round
-    """
-    extract_dir = filemanager.download_dataset(competition_id)
-    tournament = pd.read_csv(os.path.join(extract_dir, "numerai_tournament_data.csv"))
-    val = tournament[tournament["data_type"] == "validation"]
-    test = tournament[tournament["data_type"] == "test"]
-    live = tournament[tournament["data_type"] == "live"]
-
-    return list(val["id"]), list(test["id"]), list(live["id"])
-
-def get_sorted_split(data, val_ids, test_ids, live_ids):
-    """Split the competition data into validation, test, and live data sets in a sorted fashion
-
-    Parameters:
-    -----------
-    data : DataFrame
-        Tournament data for the competition round
-
-    val_ids : list
-        List of all validation data ids
-
-    test_ids : list
-        List of all test data ids
-
-    live_ids : list
-        List of all live data ids
-
-
-    Returns:
-    --------
-    validation : ndarray
-        Validation data features sorted by id
-
-    test : ndarray
-        Test data features sorted by id
-
-    live : ndarray
-        Live data features sorted by id
-    """
-    validation = data[data["id"].isin(val_ids)]
-    test = data[data["id"].isin(test_ids)]
-    live = data[data["id"].isin(live_ids)]
-
-    validation = validation.sort_values("id")
-    test = test.sort_values("id")
-    live = live.sort_values("id")
-
-    if any(["feature" in c for c in list(validation)]):
-        f = [c for c in list(validation) if "feature" in c]
-    else:
-        f = ["probability"]
-    validation = validation[f]
-    test = test[f]
-    live = live[f]
-
-    return validation.as_matrix(), test.as_matrix(), live.as_matrix()
-
-@functools.lru_cache(maxsize=2)
-def get_competition_variables(competition_id, db_manager, filemanager):
-    """Return the K-Means Clustered tournament data for the competition round
+def get_competition_split(competition_id, db_manager, filemanager, sample_fraction=.25):
+    """Return competition data that is split by the 'data_type' (validation, test)
 
     Parameters:
     -----------
@@ -181,42 +66,26 @@ def get_competition_variables(competition_id, db_manager, filemanager):
 
     Returns:
     --------
-    variables : dictionary
-        Holds clustered tournament data and the competition_id
+    validation_data : DataFrame
+        all validation data from the tournament data for the current competition_id
+
+    test_data : DataFrame
+        all test data from the tournament data for the current competition_id
+
     """
     extract_dir = filemanager.download_dataset(competition_id)
 
     training = pd.read_csv(os.path.join(extract_dir, "numerai_training_data.csv"))
     tournament = pd.read_csv(os.path.join(extract_dir, "numerai_tournament_data.csv"))
 
-    val_ids, test_ids, live_ids = get_ids(filemanager, competition_id)
-    return get_competition_variables_from_df(competition_id, training, tournament, val_ids, test_ids, live_ids)
+    validation = tournament_data[tournament_data.data_type == 'validation']
+    test = tournament_data[tournament_data.data_type == 'test']
+
+    return validation, test.sample(frac=sample_fraction,axis='row')
 
 
-def get_competition_variables_from_df(
-        competition_id: str, training: pd.DataFrame, tournament: pd.DataFrame,
-        val_ids: list, test_ids: list, live_ids: list) -> dict:
-
-    f = [c for c in list(tournament) if "feature" in c]
-
-    # TODO the dropna is a hack workaround for https://github.com/numerai/api-ml/issues/68
-    X = training[f].dropna().as_matrix()
-    X = np.append(X, tournament[f].as_matrix(), axis=0)
-
-    X_1, X_2, X_3 = get_sorted_split(tournament, val_ids, test_ids, live_ids)
-    c1, c2, c3 = make_clusters(X, X_1, X_2, X_3)
-
-    variables = {
-        "competition_id":competition_id,
-        "cluster_1" : c1,
-        "cluster_2" : c2,
-        "cluster_3" : c3,
-    }
-    return variables
-
-
-def get_submission_pieces(submission_id, competition_id,  db_manager, filemanager):
-    """Get validation, test, and live ids sorted from submission_id
+def get_submission_split(submission_id, competition_id, validation_ids, test_ids, db_manager, filemanager):
+    """Get validation and test data for a submission based on the submission_id
 
     Parameters:
     -----------
@@ -226,6 +95,12 @@ def get_submission_pieces(submission_id, competition_id,  db_manager, filemanage
     competition_id : int
         Numerical ID of the competition round of the tournament
 
+    validation_ids : ndarray
+        array of the ids belonging to competition validation data
+
+    test_ids : ndarray
+        array of the ids belonging to competition test data
+
     db_manager : DatabaseManager
         MongoDB data access object that has read and write functions to NoSQL DB
 
@@ -234,21 +109,21 @@ def get_submission_pieces(submission_id, competition_id,  db_manager, filemanage
 
     Returns:
     --------
-    validation : ndarray
-        Sorted validation ids from submission data
+    val_sub : DataFrame
+        validation data from submission data
 
-    tests : ndarray
-        Sorted test ids from submission data
+    test_sub : DataFrame
+        test data from submission data
 
-    live : ndarray
-        Sorted live ids from submission data
     """
     s3_file = db_manager.get_filename(submission_id)
     local_file = filemanager.download([s3_file])[0]
     data = pd.read_csv(local_file)
-    val_ids, test_ids, live_ids = get_ids(filemanager, competition_id)
-    validation, tests, live = get_sorted_split(data, val_ids, test_ids, live_ids)
-    return validation, tests, live
+
+    val_sub = data[data.id.isin(validation_ids)]
+    test_sub = data[data.id.isin(test_ids)]
+
+    return val_sub, test_sub
 
 def submission_concordance(submission, db_manager, filemanager):
     """Determine if a submission is concordant and write the result to MongoDB
@@ -264,21 +139,28 @@ def submission_concordance(submission, db_manager, filemanager):
     filemanager : FileManager
             S3 Bucket data access object for querying competition datasets
     """
-    s = db_manager.db.submissions.find_one({'_id':ObjectId(submission["submission_id"])})
+    s = db_manager.db.submissions.find_one({'_id':ObjectId(submission_id)})
     submission['user'] = s['username']
     submission['competition_id'] = s['competition_id']
 
-    clusters = get_competition_variables(submission['competition_id'], db_manager, filemanager)
-    P1, P2, P3 = get_submission_pieces(submission['submission_id'], submission['competition_id'], db_manager, filemanager)
-    c1, c2, c3 = clusters["cluster_1"], clusters["cluster_2"], clusters["cluster_3"]
+    submission_id = submission['submission_id']
+    competition_id = submission['competition_id']
+
+    # tournament data
+    validation, test = get_competition_split(competition_id, db_manager, filemanager)
+
+    validation_ids = validation.id.values
+    test_ids = test.id.values
+
+    # submission data
+    sub_val, sub_test = get_submission_split(submission_id, competition_id, validation_ids, test_ids, db_manager, file_manager)
 
     try:
-        concordance = has_concordance(P1, P2,P3, c1, c2, c3)
-    except IndexError:
+        concordance = has_concordance(validation.target.values, sub_val.probability.values, test.target.value, sub_test.probability.values)
+    except IndexError, ValueError:
         # If we had an indexing error, that is because the round restart, and we need to try getting the new competition variables.
-         get_competition_variables.cache_clear()
-         clusters = get_competition_variables(submission['competition_id'], db_manager, filemanager)
-         c1, c2, c3 = clusters["cluster_1"], clusters["cluster_2"], clusters["cluster_3"]
-         concordance = has_concordance(P1, P2,P3, c1, c2, c3)
+         get_competition_split.cache_clear()
+         validation, test = get_competition_split(competition_id, db_manager, filemanager)
+         concordance = has_concordance(validation.target.values, sub_val.probability.values, test.target.value, sub_test.probability.values)
 
-    db_manager.write_concordance(submission['submission_id'], submission['competition_id'], concordance)
+    db_manager.write_concordance(submsission_id, competition_id, concordance)
